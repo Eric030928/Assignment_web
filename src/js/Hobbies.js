cf79db94f7f504ae204fad3c6100ab9bfc465ff2
Eric030928 <2873804973@qq.com>
2024-05-07 19:45:53 +0800

    Update 8

--- src/js/Hobbies.js
+++ src/js/Hobbies.js
@@ -1,35 +1,37 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  // 调用函数为第一个容器应用代码
-  applySlideshow('.slideshow-container-1');
-  
-  // 调用函数为第二个容器应用代码
-  applySlideshow('.slideshow-container-2');
+document.addEventListener("DOMContentLoaded", function (event) {
+    // 调用函数为第一个容器应用代码
+    applySlideshow('.slideshow-container-1');
+
+    // 调用函数为第二个容器应用代码
+    applySlideshow('.slideshow-container-2');
+
+    applySlideshow('.slideshow-container-3');
 });
 
 function applySlideshow(containerSelector) {
-  const container = document.querySelector(containerSelector);
-  const slides = container.querySelectorAll('.slide');
-  let currentSlide = 0;
+    const container = document.querySelector(containerSelector);
+    const slides = container.querySelectorAll('.slide');
+    let currentSlide = 0;
 
-  function showSlide(index) {
-      slides.forEach((slide, i) => {
-          if (i === index) {
-              slide.classList.add('active');
-          } else {
-              slide.classList.remove('active');
-          }
-      });
-  }
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            if (i === index) {
+                slide.classList.add('active');
+            } else {
+                slide.classList.remove('active');
+            }
+        });
+    }
 
-  function nextSlide() {
-      currentSlide++;
-      if (currentSlide >= slides.length) {
-          currentSlide = 0;
-      }
-      showSlide(currentSlide);
-  }
+    function nextSlide() {
+        currentSlide++;
+        if (currentSlide >= slides.length) {
+            currentSlide = 0;
+        }
+        showSlide(currentSlide);
+    }
 
-  setInterval(nextSlide, 2000); // 每 2 秒切换一张幻灯片
+    setInterval(nextSlide, 2000); // 每 2 秒切换一张幻灯片
 
-  showSlide(currentSlide); // 显示第一张幻灯片
+    showSlide(currentSlide); // 显示第一张幻灯片
 }
--- src/js/Welcome.js
+++ src/js/Welcome.js
@@ -1,7 +1,7 @@
 function learn_hover(obj) {
-	obj.innerHTML="Learn about me! >>"
-  }
+  obj.innerHTML = "Learn about me! >>"
+}
 
-  function learn_hover_out(obj){
-	obj.innerHTML="Learn about me!"
+function learn_hover_out(obj) {
+  obj.innerHTML = "Learn about me!"
 }
